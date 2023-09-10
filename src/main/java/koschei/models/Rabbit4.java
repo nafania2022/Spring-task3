521c07deabacd4ffcc336c4f0c220bfc48e4981a
Nafania2022 <nafania1522@gmail.com>
2023-09-10 16:57:43 +0300

    update 0.0.1

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    Duck5 duck;
+    @Autowired
+    public Rabbit4(Duck5 duck5) {
+        this.duck=duck5;
+    }
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
