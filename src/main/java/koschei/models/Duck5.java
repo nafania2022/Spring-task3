278452ffdc845c6d79de27cf6b42ff00e5b03531
Nafania2022 <nafania1522@gmail.com>
2023-09-19 17:17:01 +0300

    update

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -6,12 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Duck5 {
 
-
-    Egg6 egg;
     @Autowired
-    public Duck5(Egg6 egg) {
-        this.egg=egg;
-    }
+    Egg6 egg;
+
+
     @Override
     public String toString() {
         return ", в утке яйцо " + egg.toString();
